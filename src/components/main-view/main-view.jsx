a679017a3d2343c8fead56a33d45a25054eb7890
sabine-mansour <sabine_mansour@icloud.com>
2021-04-26 14:52:04 +0200

    Fetched movies from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,13 +9,19 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Psycho', Description: 'Marion disappears after stealing money from her employer. Her lover and sister try to find her and end up reaching the infamous Bates Motel, where they meet Norman Bates.', ImagePath: 'https://resizing.flixster.com/v_K71ZmJSwUOq07BPt5lkvDv8hE=/206x305/v2/https://flxt.tmsimg.com/assets/p283_p_v13_an.jpg' },
-        { _id: 1, Title: 'This is 40', Description: "Pete and Debbie are both about to turn 40, their kids hate each other, both of their businesses are failing, they're on the verge of losing their house, and their relationship is threatening to fall apart.", ImagePath: 'https://resizing.flixster.com/pXfXz0gkQPCVEhuA-aAK78QdKMM=/206x305/v2/https://flxt.tmsimg.com/assets/p9286982_p_v10_aa.jpg' },
-        { _id: 1, Title: 'Predator', Description: 'Dutch and his team are out on a mission to rescue a group of hostages in Central America. There, they discover that they are being targeted by an extraterrestrial warrior.', ImagePath: 'https://resizing.flixster.com/Zk1DHum0Aru8gNYGFIqwgnjQlFg=/206x305/v2/https://flxt.tmsimg.com/assets/p10094_p_v13_ah.jpg' }
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('https://flixinfo.herokuapp.com/movies').then(response => {
+      this.setState({
+        movies: response.data
+      });
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -25,7 +33,7 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0) return <div className="main-view">This list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
 
 
     return (
